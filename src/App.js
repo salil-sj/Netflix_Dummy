ddd85a667874487bab1f173c5ef74fb978bc26e2
Salil Joshi <salilsj1619@gmail.com>
2023-09-01 00:20:28 +0530

    Login/Sign page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import Body from "./components/Body";
 
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-600">
-      Hello world
-    </div>
+   <Body/>
   );
 }
 
